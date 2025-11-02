1981bfd9e37ef37f256e99c6095d182aadbac39e
MH-Oliver <oliver.jockel@gmx.de>
2025-11-02 16:19:27 +0100

    Cpp Code added

--- ros-actuator-wheels/launchers/default.sh
+++ ros-actuator-wheels/launchers/default.sh
@@ -13,7 +13,7 @@ dt-launchfile-init
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
 
 # launching app
-dt-exec rosrun example_ros_wheels main.py
+dt-exec rosrun example_ros_wheels driver_cpp_node
 
 
 # ----------------------------------------------------------------------------
