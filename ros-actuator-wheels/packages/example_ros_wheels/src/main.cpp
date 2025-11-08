f4b54f8e3a5afc293991697d26e7a789dcc441fd
MH-Oliver <oliver.jockel@gmx.de>
2025-11-08 17:59:32 +0100

    Anderen Node angreifen

--- ros-actuator-wheels/packages/example_ros_wheels/src/main.cpp
+++ ros-actuator-wheels/packages/example_ros_wheels/src/main.cpp
@@ -1,25 +1,23 @@
 #include <ros/ros.h>
-// NEUER NACHRICHTENTYP:
-#include <duckietown_msgs/Twist2DStamped.h>
+#include <duckietown_msgs/WheelsCmdStamped.h>
 #include <string>
-#include <cstdlib>
+#include <cstdlib> // Für std::getenv
 #include <ros/time.h>
 #include <ros/duration.h>
 
 // === Parameter ===
 const double DURATION = 20.0;
-// Dies ist jetzt die lineare Geschwindigkeit in m/s
 const double SPEED = 0.2;
 
 /**
- * @brief Sendet einen (0, 0) Befehl (Stop) an das High-Level Topic.
- * @param publisher Der ROS-Publisher für die Steuerbefehle.
+ * @brief Sendet einen (0, 0) Befehl und wartet 1 Sekunde.
+ * @param publisher Der ROS-Publisher für die Radbefehle.
  */
-void stop_robot(ros::Publisher& publisher) {
-    duckietown_msgs::Twist2DStamped msg;
+void stop_wheels(ros::Publisher& publisher) {
+    duckietown_msgs::WheelsCmdStamped msg;
     msg.header.stamp = ros::Time::now();
-    msg.v = 0.0;
-    msg.omega = 0.0;
+    msg.vel_left = 0.0;
+    msg.vel_right = 0.0;
     publisher.publish(msg);
     ros::Duration(1.0).sleep();
 }
@@ -28,7 +26,6 @@ void stop_robot(ros::Publisher& publisher) {
  * @brief Haupt-Driver-Funktion
  */
 int driver(int argc, char **argv) {
-    // Umgebungsvariable VEHICLE_NAME wird immer noch benötigt
     const char* robot_name_env = std::getenv("VEHICLE_NAME");
     if (robot_name_env == nullptr) {
         ROS_FATAL("Umgebungsvariable VEHICLE_NAME nicht gesetzt. (Mit -R [name] starten)");
@@ -40,38 +37,31 @@ int driver(int argc, char **argv) {
     ros::init(argc, argv, "driver", ros::init_options::AnonymousName);
     ros::NodeHandle n;
 
-    // === NEUES TOPIC & NEUER NACHRICHTENTYP ===
-    // Wir verwenden das exakte Topic aus deinem "rosnode info" Log
-    std::string topic_name = "/zeta/lane_controller_node/car_cmd";
+    // Das direkte, Low-Level RÄDER-Topic
+    std::string topic_name = "/" + robot_name + "/wheels_driver_node/wheels_cmd";
+    ros::Publisher publisher = n.advertise<duckietown_msgs::WheelsCmdStamped>(topic_name, 1);
 
-    ros::Publisher publisher = n.advertise<duckietown_msgs::Twist2DStamped>(topic_name, 1);
-    // === ÄNDERUNG ENDE ===
-
-    ROS_INFO("Warte auf Verbindung zum Publisher...");
     ros::Duration(0.5).sleep();
 
     // Bereite die "drive_msg" vor
-    duckietown_msgs::Twist2DStamped drive_msg;
-    drive_msg.v = SPEED;     // v = lineare Geschwindigkeit (geradeaus)
-    drive_msg.omega = 0.0;   // omega = Winkelgeschwindigkeit (keine Drehung)
+    duckietown_msgs::WheelsCmdStamped drive_msg;
+    drive_msg.vel_left = SPEED;
+    drive_msg.vel_right = SPEED;
 
-    // Wir verwenden ros::Rate für eine saubere Schleife (z.B. 50 Hz)
-    ros::Rate loop_rate(50);
+    // Eine saubere Frequenz von 30 Hz ist völlig ausreichend
+    ros::Rate loop_rate(30);
 
     ros::Time stime = ros::Time::now();
     ROS_INFO("Starte Fahrt auf Topic '%s' für %.1f Sekunden...", topic_name.c_str(), DURATION);
 
     while (ros::ok() && (ros::Time::now() - stime).toSec() < DURATION) {
-        // Wir müssen den Header-Zeitstempel bei jeder Sendung aktualisieren
         drive_msg.header.stamp = ros::Time::now();
         publisher.publish(drive_msg);
-
-        // Warte "den Rest" der 1/50 Sekunde
         loop_rate.sleep();
     }
 
-    ROS_INFO("Dauer abgelaufen. Stoppe Roboter.");
-    stop_robot(publisher);
+    ROS_INFO("Dauer abgelaufen. Stoppe Räder.");
+    stop_wheels(publisher);
 
     return 0;
 }
