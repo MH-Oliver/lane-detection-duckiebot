0e40e7f997b7ca5816a427766075ce3bfe7310c2
MH-Oliver <oliver.jockel@gmx.de>
2025-12-15 11:25:05 +0100

    Parameter angepasst

--- ros-actuator-wheels/packages/example_ros_wheels/src/main.cpp
+++ ros-actuator-wheels/packages/example_ros_wheels/src/main.cpp
@@ -197,7 +197,7 @@ int driver(int argc, char **argv) {
 
     // Timer für die Bildverarbeitung (0.5s Takt)
     ros::Time last_process_time = ros::Time::now();
-    const double PROCESS_INTERVAL = 0.5; // Sekunden
+    const double PROCESS_INTERVAL = 0.25; // Sekunden
 
     ros::Time start_time = ros::Time::now();
     while (start_time.toSec() == 0) {
