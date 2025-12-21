0fd4aaf7c1aa7cd5eaf3c70d2b7f13244167a430
MH-Oliver <oliver.jockel@gmx.de>
2025-12-21 18:34:30 +0100

    fix 2 (ColorSpaceScaling aufgeräumt)

--- ros-actuator-wheels/packages/example_ros_wheels/src/core/PipelineChuckNorris/A/ColorSpaceScaling.h
+++ ros-actuator-wheels/packages/example_ros_wheels/src/core/PipelineChuckNorris/A/ColorSpaceScaling.h
@@ -1,24 +1,25 @@
 #ifndef COLORSPACESCALING_H
 #define COLORSPACESCALING_H
+
 #include <opencv2/opencv.hpp>
+
 using namespace cv;
 using namespace std;
+
 class ColorSpaceScaling {
 public:
     static int m_imageFlagYuv;
-    static Mat m_image; // Das BGR-Bild, auf das wir zeichnen
+    static Mat m_image;
 
     static void greyScaleBGRTOGREY();
-
     static void backYUVToRGB();
-
     static void yuvScale();
 
-    // NEUE SIGNATUR: Nimmt das YUV-Bild zum Lesen entgegen
-    static void drawLine( int k, int step, int y_line, int starlefside,const Mat& yuvImage);
+    static void drawLine(int k, int step, int y_line, int starlefside, const Mat& yuvImage);
 
     static void verticalThreeFourthLine();
+
     static Mat process(Mat image);
 };
 
-#endif //COLORSPACESCALING_H
+#endif
