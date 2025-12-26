907f08f586cd393312253d207a02261304e67680
MH-Oliver <oliver.jockel@gmx.de>
2025-12-26 14:52:04 +0100

    fix

--- ros-actuator-wheels/packages/example_ros_wheels/src/main.cpp
+++ ros-actuator-wheels/packages/example_ros_wheels/src/main.cpp
@@ -19,7 +19,7 @@
 // ===  HIER WÄHLEN VOM VERFAHREN  ===
 // ==========================================
 // Kommentiere diese Zeile aus (//), um das ALTE Verfahren zu nutzen.
-#define USE_NEW_PIPELINE
+//#define USE_NEW_PIPELINE
 
 #ifdef USE_NEW_PIPELINE
     #include "core/PipelineChuckNorris/LineDetectionPipeline.h"
@@ -344,6 +344,9 @@ int driver(int argc, char **argv) {
         }
         loop_rate.sleep();
     }
+
+	double rate = 100.0 * (double)successful_frames / total_frames_processed;
+    ROS_INFO("Finaler-Verfahren-Score: %.1f %% Frames mit 2 Linien", rate);
 
     stop_wheels(publisher);
     return 0;
