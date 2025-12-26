4ecec5bb88e89833e1b02559a931ed4524cd0436
MH-Oliver <oliver.jockel@gmx.de>
2025-12-26 19:38:24 +0100

    länger

--- ros-actuator-wheels/packages/example_ros_wheels/src/core/runtime_config.cpp
+++ ros-actuator-wheels/packages/example_ros_wheels/src/core/runtime_config.cpp
@@ -1,6 +1,6 @@
 #include "runtime_config.h"
 
-double RuntimeConfig::execution_duration = 30.0;
+double RuntimeConfig::execution_duration = 60.0;
 
 void RuntimeConfig::setDuration(double seconds) {
     execution_duration = seconds;
